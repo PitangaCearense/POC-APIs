7cb03cbbc38ef472c2d880a7731f7ed974495f02
ronaldogomes96 <gomes.ronaldo96@gmail.com>
2021-05-17 12:01:12 -0300

    Initial setup for the project with alomafire

---
+++ POC_APIs/POC_APIs/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  POC_APIs
+//
+//  Created by Ronaldo Gomes on 17/05/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
